42f00c5011d474fbe1e66d6bdfa32d26780ccbb2
kaushalendrapandey05 <126902325+Kaushalendra-pandey-05@users.noreply.github.com>
2024-09-09 18:52:21 +0530

    Update App.jsx

--- password/genratepassword/src/App.jsx
+++ password/genratepassword/src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./styles.css";
+import "./styles/tailwind-pre-build.css";
+import { useState, useCallback, useEffect, useRef } from "react";
+export default function App() {
+  const [length, setLength] = useState(8);
+  const [numaAll, setNumAll] = useState(false);
+  const [charaAll, setcharAll] = useState(false);
+  const [password, setPassword] = useState("");
+  //useRef Hook
+  const passwordRef = useRef(null);
 
-function App() {
-  const [count, setCount] = useState(0)
+  const PasswordGenrator = useCallback(() => {
+    let pass = "";
+    let char = "abcdefghijklmnopqrstuvwxyz";
+    if (numaAll) char += "1234567890";
+    if (charaAll) char += "!@$%^&*()_+{}";
+
+    for (let i = 1; i <= length; i++) {
+      let str = Math.floor(Math.random() * char.length + 1);
+
+      pass += char.charAt(str);
+    }
+    setPassword(pass);
+  }, [length, numaAll, charaAll, setPassword]);
+
+  const copyToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    PasswordGenrator();
+  }, [length, numaAll, charaAll]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white">Password genrator</h1>
+        <div className='className="flex shadow rounded-lg overflow-hidden mb-4"'>
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyToClipboard}
+            className="outline-none bg-blue-700 text-white px- py-0.5 shrink-0"
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label> Length: ({length}) </label>
+          </div>
+          <div className="flex items-center gap-x-3">
+            <input
+              type="checkbox"
+              defaultChecked={numaAll}
+              id="numberInput"
+              onChange={() => {
+                setNumAll((prev) => !prev);
+              }}
+            />
+            <label> Number: </label>
+          </div>
+          <div className="flex items-center gap-x-10">
+            <input
+              type="checkbox"
+              defaultChecked={charaAll}
+              id="numberInput"
+              onChange={() => {
+                setcharAll((prev) => !prev);
+              }}
+            />
+            <label> Character: </label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
